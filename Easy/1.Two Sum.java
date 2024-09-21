ef5c97d705cede2856eb939bebd161a4010ecee7
Suresh Gundumogula <104331245+Suresh4723@users.noreply.github.com>
2024-09-21 23:43:24 +0530

    Update 1.Two Sum.java

--- Easy/1.Two Sum.java
+++ Easy/1.Two Sum.java
@@ -2,12 +2,14 @@ class Solution {
    public int[] twoSum(int[] nums, int target) {
       int[] solution={0,0};
       Map<Integer, Integer> solutionmap= new HashMap<>();
-      for(int i=0;i<nums.length;i++){
-         int diff-target-nums[i];
-         if(solutionmap.containsKey(diff)){
-            return new int[]{i, solutionmap.get(diff)};
-         }
-         solutionmap.put(nums[i], i);
+      for(int i=0;i<nums.length;i++)
+      {
+           int diff-target-nums[i];
+           if(solutionmap.containsKey(diff))
+           {
+           return new int[]{i, solutionmap.get(diff)};
+           }
+           solutionmap.put(nums[i], i);
       }
       return null;
    }
