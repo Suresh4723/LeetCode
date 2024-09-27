72aca58e071aea4662d002aa968a3c3f0c4a5e91
Suresh Gundumogula <104331245+Suresh4723@users.noreply.github.com>
2024-09-27 23:38:50 +0530

    Update 287. Find the Duplicate Number.java

--- Medium/287. Find the Duplicate Number.java
+++ Medium/287. Find the Duplicate Number.java
@@ -1,1 +1,14 @@
-
+class Solution {
+    public int findDuplicate(int[] nums) {
+        Set<Integer> hs=new HashSet<>();
+        for(int i=0;i<nums.length;i++)
+        {
+            if(hs.contains(nums[i]))
+            {
+                return nums[i];
+            }
+            hs.add(nums[i]);
+        }
+    return -1;
+    }
+}
